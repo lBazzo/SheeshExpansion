4fac9cfcd8bc3fcfcfde55f56f48eedf9109fa9a
lBazzo <bastien.poole@gmail.com>
2026-01-17 15:09:52 +0000

    17/01/2026 04 - fixed sprites for CooltrainerF and Red Gyarados (ty grint) + changed slowpoke to have tails... hopefully no one sees

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -369,6 +369,9 @@ $(OBJEVENTGFXDIR)/people/man.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/cooltrainer_m.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
+$(OBJEVENTGFXDIR)/people/cooltrainer_f.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
+
 $(OBJEVENTGFXDIR)/people/rocker.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
