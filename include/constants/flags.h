9fed49cdd493512a24397fd910295005cd2b9b2b
lBazzo <bastien.poole@gmail.com>
2026-05-09 13:50:48 +0100

    09/05/2026 01 - Updated entirety of lighthouse, moved some trainers about from elsewhere

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -758,7 +758,7 @@
 #define FLAG_DEFEATED_GOLDENROD_CITY_SILVER  0x2AF // Unused Flag
 #define FLAG_DEFEATED_RADIO_TOWER_ARCHER  0x2B0 // Unused Flag
 #define FLAG_DEFEATED_VICTORY_ROAD_SILVER  0x2B1 // Unused Flag
-#define FLAG_UNUSED_0x2B2  0x2B2 // Unused Flag
+#define FLAG_LIGHTHOUSE_BREAK_USED  0x2B2 // Unused Flag
 #define FLAG_UNUSED_0x2B3  0x2B3 // Unused Flag
 #define FLAG_UNUSED_0x2B4  0x2B4 // Unused Flag
 #define FLAG_UNUSED_0x2B5  0x2B5 // Unused Flag
